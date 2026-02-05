57689f6d7e2ff7130f6922505d476039475c3c7b
Evonex <evonex200@gmail.com>
2026-02-05 13:40:05 +0100

    Add BrowserRouter and set up Routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignupPage />} />
+    </Routes>
   )
 }
 
